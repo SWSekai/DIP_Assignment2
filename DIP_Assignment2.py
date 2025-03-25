c09df9866a2141e11a3b7e01b3cd58da5a5231c4
Hans Chen <hanschen31310@gmail.com>
2025-03-25 20:23:09 +0800

    Semi-finished

--- DIP_Assignment2.py
+++ DIP_Assignment2.py
@@ -2,28 +2,51 @@ import cv2
 import numpy as np
 
 def preprocess_image(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # 灰階
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # 灰階
     
-    median_filtered = cv2.medianBlur(gray, 3) # 中值濾波(椒鹽雜訊)
+    img = cv2.equalizeHist(img) # 直方圖均衡化 - 增強對比度和細節
+    
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8)) # 自適應直方圖均衡化-處理暗部和亮部細節
+    img = clahe.apply(img)
+    
+    img = cv2.medianBlur(img, 3) # 中值濾波(椒鹽雜訊)
 
-    blurred = cv2.GaussianBlur(median_filtered, (3, 3), 0) # 高斯模糊(高頻雜訊)
+    img = cv2.GaussianBlur(img, (5, 5), 0) # 高斯模糊(高頻雜訊)
         
-    # bilateral_filtered = cv2.bilateralFilter(blurred, 9, 75, 75) # 雙邊濾波(保留邊緣)
+    img = cv2.bilateralFilter(img, 3, 5, 5) # 雙邊濾波(保留邊緣)
+    
+    gamma = 0.5  # 改善暗部細節
+    img = np.array(255 * (img / 255) ** gamma, dtype=np.uint8) # gamma校正
 
-    return blurred
+    return img
 
 def detect_edges(image, method='sobel'):
-    sobel_x = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)
-    sobel_y = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3)
+    if method.lower() == 'sobel':
+        sobel_x = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
+        sobel_y = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
 
-    sobel_edges = np.sqrt(sobel_x**2 + sobel_y**2)
-    
-    sobel_edges = cv2.normalize(sobel_edges, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-    
-    return sobel_edges
+        edge = np.sqrt(sobel_x**2 + sobel_y**2)
+        
+        edge = cv2.normalize(edge, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        
+        kernel = np.ones((3,3), np.uint8) # 簡化線條
+        
+        # edge = cv2.morphologyEx(edge, cv2.MORPH_CLOSE, kernel) # 閉運算，填補小的斷裂
+        
+        # edge = cv2.morphologyEx(edge, cv2.MORPH_OPEN, kernel)  # 開運算，去除細小雜點
+        
+    elif method.lower() == 'canny':
+        edge = cv2.Canny(image, 100, 200)
+        
+    return edge
 
-def apply_thresholding(edges, threshold_type='binary'):
-    _, binary_edges = cv2.threshold(edges, 50, 255, cv2.THRESH_BINARY)
+def apply_thresholding(edges, threshold_type='adaptive'): 
+    if threshold_type.lower() == 'binary':
+        _, binary_edges = cv2.threshold(edges, 50, 255, cv2.THRESH_BINARY_INV)
+        
+    elif threshold_type.lower() == 'adaptive':
+        binary_edges = cv2.adaptiveThreshold(edges, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 3)
+  
     
     return binary_edges
 
